f0085c9bbb93c08737ddf85ba54bfb6959677525
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-06-12 00:48:57 +0600

    ph-university-server version0.1

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -2,21 +2,33 @@ import express from 'express';
 import requestValidator from '../../middlewares/requestValidator';
 import { CourseValidation } from './course.validation';
 import { CourseControllers } from './course.controllers';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constants';
 
 
 const router = express.Router();
 
-router.post("/create-course", requestValidator(CourseValidation.createCourseValidationSchema), CourseControllers.createCorse);
+router.post("/create-course", auth(USER_ROLE.admin), requestValidator(CourseValidation.createCourseValidationSchema), CourseControllers.createCorse);
 
 router.get('/', CourseControllers.getAllCourses);
 
 router.get(
     '/:id',
+    auth(
+        USER_ROLE.admin,
+        USER_ROLE.student,
+        USER_ROLE.faculty
+    )
+    ,
     CourseControllers.getSingleCourse,
 );
 
 router.delete(
     '/:id',
+    auth(
+        USER_ROLE.admin,
+    )
+    ,
     CourseControllers.deleteCourse,
 );
 
@@ -27,7 +39,6 @@ router.put(
     ),
     CourseControllers.assignFacultiesWithCourse
 );
-
 
 
 router.delete(
@@ -43,6 +54,9 @@ router.patch(
     '/:id',
     requestValidator(
         CourseValidation.updateCreateCourseValidationSchema,
+    ),
+    auth(
+        USER_ROLE.admin,
     ),
     CourseControllers.updateCourse,
 );
